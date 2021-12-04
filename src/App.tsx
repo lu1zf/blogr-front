aa957bcb10c2ffc65f9f315840d3ec8cb5cb3d38
Luiz Silva <luizsilva.ee@gmail.com>
2021-12-04 17:02:24 -0300

    Adiciona componentes ao App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { GlobalStyle } from "./styles/global";
+import { Header } from "./components/Header";
+
 function App() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <Header />
+      <GlobalStyle />
     </div>
   );
 }
